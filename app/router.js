d92261ab1bb79fed8c95d6df0da7cc53c70f6d93
Francesca Garbagnati <fgarbagnati@Francescas-MacBook-Pro.local>
2017-01-18 00:17:42 -0800

    Plays around with routing, templates, route's models

--- app/router.js
+++ app/router.js
@@ -3,10 +3,19 @@ import config from './config/environment';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  // rootURL: config.rootURL
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('services');
+  this.route('contact');
+
+  this.resource('posts', {path: '/posts'}, function () {
+      this.route('new');
+      this.route('post', {path: ':post_id'});
+  });
+  this.route('events');
 });
 
 export default Router;
