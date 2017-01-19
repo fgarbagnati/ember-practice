0b520b7cc97afcde23a598263fe04d7dfdcc1769
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-18 22:41:38 -0800

    Comments out old post stuff

--- app/router.js
+++ app/router.js
@@ -11,10 +11,10 @@ Router.map(function() {
   this.route('services');
   this.route('contact');
 
-  this.resource('posts', {path: '/posts'}, function () {
-      this.route('new');
-      this.route('post', {path: ':post_id'});
-  });
+  // this.resource('posts', {path: '/posts'}, function () {
+  //     this.route('new');
+  //     this.route('post', {path: ':post_id'});
+  // });
   this.route('events');
 });
 
