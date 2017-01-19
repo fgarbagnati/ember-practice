d00310d1880e7f5413c3ee92b722da26b568d67f
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-18 22:49:55 -0800

    Adds missing semicolon

--- app/routes/services.js
+++ app/routes/services.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return ['PC Repair', 'Network Diagnostics', 'Home Audio Installations']
+		return ['PC Repair', 'Network Diagnostics', 'Home Audio Installations'];
 	}
 });
